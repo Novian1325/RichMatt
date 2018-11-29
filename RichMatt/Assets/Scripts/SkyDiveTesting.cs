d3188d1d045b9aea19578b884f89799584a21953
Matt <mentallic3d@gmail.com>
2018-11-28 19:06:23 -0500

    Added Inital Skydiving
    Rich will help me figure it out

---
+++ RichMatt/Assets/Scripts/SkyDiveTesting.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyDiveTesting : MonoBehaviour
+{
+	[Header("**Level Designer Only**")]
+	public float MaxUpAngle = 9.0f;
+	public float MaxDownAngle = 45.0f;
+	public float ChuteHeight = 100f;
+	public float ChuteDrag = 3.65f;
+	public float RotationSpeed = 145f;
+
+	[Range(5f, 50f)]
+   public float FallingDragTuning = 30.0f;
+
+	private Rigidbody rb;
+	private float distanceToTerrain;
+	private bool charFalling = true;
+    private Animator anim;
+
+	void Start ()
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+        anim = gameObject.GetComponent<Animator>();
+
+       // rb.AddForce(new Vector3(0,0,1), ForceMode.Impulse);
+        //rb.velocity = (transform.forward * 1.5f);
+
+        //normalize these variables - this permits the level Designer to be carefree about setting these
+        if (MaxDownAngle > 89.5f)
+		  MaxDownAngle = (90 - MaxDownAngle) + 1;
+		MaxUpAngle = Mathf.Abs(MaxUpAngle);
+		MaxDownAngle = Mathf.Abs(MaxDownAngle);
+	}
+
+	void Update ()
+	{
+        anim.SetBool("SkyDive", charFalling);
+
+        /*
+		if (Input.GetAxisRaw ("Mouse Y") > 0)
+        {
+			//Twist to the right
+			var angle = transform.rotation.eulerAngles.x;
+			if ((angle < MaxDownAngle) || (angle > 180.0f))
+			  transform.Rotate (Vector3.right * RotationSpeed * Time.deltaTime);
+		}
+        else if (Input.GetAxisRaw ("Mouse Y") < 0)
+		{
+			//Twist to the left
+			var angle = transform.rotation.eulerAngles.x;
+			if ((angle > MaxUpAngle) && (angle < 180.0f))
+			  transform.Rotate (Vector3.right * -(RotationSpeed * Time.deltaTime));
+		}
+            transform.Rotate(Vector3.up * (Input.GetAxisRaw("Mouse X") * 10.0f) * Time.deltaTime);
+            */
+    
+        /*
+        
+        //Skydiving
+        //Mouse Left and Right: = rotate character on Y axis
+        if (Input.GetAxisRaw("Mouse X") > 0)
+        {
+            //Twist to the right
+                transform.Rotate(Vector3.up * RotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetAxisRaw("Mouse X") < 0)
+        {
+            //Twist to the left
+                transform.Rotate(Vector3.up * -(RotationSpeed * Time.deltaTime));
+        }
+        
+    */
+
+        //Skydiving
+        //Key left, key right = rotate on the z axis
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            //Dive
+            var angle = transform.rotation.eulerAngles.x;
+            if ((angle < MaxDownAngle) || (angle > 180.0f))
+                transform.Rotate(Vector3.right * RotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            //Pull Back
+            var angle = transform.rotation.eulerAngles.x;
+            if ((angle < MaxUpAngle) && (angle < 180.0f))
+                transform.Rotate(Vector3.right * -(RotationSpeed * Time.deltaTime));
+               // transform.Rotate(Vector3.right * -(RotationSpeed * Time.deltaTime));
+            //transform.Rotate((Vector3.right.x * -(RotationSpeed * Time.deltaTime)), transform.rotation.y, transform.rotation.z);
+        }
+
+        transform.Rotate(Vector3.up * (Input.GetAxisRaw("Vertical") * 10.0f) * Time.deltaTime);
+
+        /*
+
+
+        //Skydiving
+        //Key Forward = dive
+        //Key backwards = pull back
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            //Twist to the right
+            //transform.Rotate(new Vector3(0, 0, (Vector3.forward.z * RotationSpeed)) * Time.deltaTime);
+            //transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, (Vector3.forward.z * RotationSpeed)) * Time.deltaTime);
+            //transform.rotation = Quaternion.Euler(0, 0, 0);
+            //transform.Rotate(Vector3.forward * RotationSpeed * Time.deltaTime, Space.Self);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            //Twist to the left
+            //transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, transform.rotation.y, (Vector3.forward.z * -RotationSpeed)) * Time.deltaTime);
+            //transform.Rotate(Vector3.forward * -(RotationSpeed * Time.deltaTime), Space.Self);
+            // transform.Rotate(new Vector3(0, 0, (Vector3.forward.z * -RotationSpeed)) * Time.deltaTime);
+        }
+
+        */
+
+
+    }
+
+	private void DistanceToTerrain()
+	{
+		RaycastHit hit;
+
+		if (Physics.Linecast(transform.position, -Vector3.up, out hit)) {
+			distanceToTerrain = hit.distance;
+		}
+	}
+
+	private float CalculateDragIncrement()
+	{
+        float Cosmic;
+        Cosmic = 0.025f;
+		return (Mathf.Abs(90f - transform.rotation.eulerAngles.x) * Cosmic) * FallingDragTuning;
+	}
+
+  void FixedUpdate()
+	{
+		DistanceToTerrain();
+
+		if (charFalling)
+        {
+		  if (distanceToTerrain <= ChuteHeight)
+            {
+			  rb.drag = ChuteDrag;
+			  charFalling = false;
+		  } else
+            {
+		    rb.drag = CalculateDragIncrement();
+		  }
+		}
+	}
+
+}
---
+++ RichMatt/Assets/Scripts/SmoothFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+	public Transform followTarget;
+	public float smoothSpeed = 10.0f;
+	public float smoothRot = 1.0f;
+	public Transform followOffset;
+
+	void LateUpdate()
+	{
+		//Smoothly move to stay at the offset
+		Vector3 desiredPosition = followOffset.transform.position;
+		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+		transform.position = smoothedPosition;
+
+		//Smoothly rotate to stay "behind"
+		Quaternion smoothedRot = Quaternion.Slerp(transform.rotation, followTarget.transform.rotation, smoothRot * Time.deltaTime);
+		transform.rotation = smoothedRot;
+	}
+}
