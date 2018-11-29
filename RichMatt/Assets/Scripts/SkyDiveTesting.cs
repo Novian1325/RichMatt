bf1f2783c084edece5f7a33d8b13fbfc3f8ce99e
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-11-28 18:38:31 -0800

    benchmark commit
    why u no rotate nicely?

--- RichMatt/Assets/Scripts/SkyDiveTesting.cs
+++ RichMatt/Assets/Scripts/SkyDiveTesting.cs
@@ -4,40 +4,103 @@ using UnityEngine;
 
 public class SkyDiveTesting : MonoBehaviour
 {
-	[Header("**Level Designer Only**")]
+    public SkyDivingStateENUM skyDivingState = SkyDivingStateENUM.startFreeFalling;
+
+    [Header("**Level Designer Only**")]
 	public float MaxUpAngle = 9.0f;
 	public float MaxDownAngle = 45.0f;
 	public float ChuteHeight = 100f;
 	public float ChuteDrag = 3.65f;
 	public float RotationSpeed = 145f;
+    public float ForwardSpeed = 5f;//player moves forward while falling
+    
+    //private readonly float _CameraDistance = 10f;
+    private Vector3 _LocalRotation = Vector3.zero;//starting 
+    public Transform cameraPivotTransform;
+    [Header("Camera Controls")]
+    public float MouseSensitivity = 4.0f;
+    public float ScrolSensitivity = 2.0f;
+    public float OrbitDampening = 10.0f;
+    public float ScrollDampening = 6.0f;
 
-	[Range(5f, 50f)]
-   public float FallingDragTuning = 30.0f;
+    [Range(5f, 50f)]
+    public float FallingDragTuning = 30.0f;
 
 	private Rigidbody rb;
 	private float distanceToTerrain;
-	private bool charFalling = true;
     private Animator anim;
-    public BRS_TPCharacter playerController;
+    private BRS_TPCharacter playerCharacter;
+    private BRS_TPController playerController;
 
-	void Start ()
-	{
-		rb = gameObject.GetComponent<Rigidbody> ();
+    private void Awake()
+    {
+        //gather references
+        if(playerCharacter == null) playerCharacter = this.gameObject.GetComponent<BRS_TPCharacter>();
+        if(playerController == null) playerController = this.gameObject.GetComponent<BRS_TPController>();
+
+        rb = gameObject.GetComponent<Rigidbody>();
         anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Start ()
+	{
+        
 
        // rb.AddForce(new Vector3(0,0,1), ForceMode.Impulse);
         //rb.velocity = (transform.forward * 1.5f);
 
-        //normalize these variables - this permits the level Designer to be carefree about setting these
-        if (MaxDownAngle > 89.5f)
-		  MaxDownAngle = (90 - MaxDownAngle) + 1;
-		MaxUpAngle = Mathf.Abs(MaxUpAngle);
-		MaxDownAngle = Mathf.Abs(MaxDownAngle);
+  //      //normalize these variables - this permits the level Designer to be carefree about setting these
+  //      if (MaxDownAngle > 89.5f)
+		//  MaxDownAngle = (90 - MaxDownAngle) + 1;
+		//MaxUpAngle = Mathf.Abs(MaxUpAngle);
+		//MaxDownAngle = Mathf.Abs(MaxDownAngle);
 	}
 
-	void Update ()
-	{
-        anim.SetBool("SkyDive", charFalling);
+    private void StartFreeFalling()
+    {
+        //the only thing that should be accepting controls is this code
+        if(playerController == null)
+        {
+            Debug.LogError("ERROR!!!!!!!");
+        }
+        else
+        {
+            playerController.enabled = false;
+
+        }
+        playerCharacter.enabled = false;
+        anim.SetBool("SkyDive", true);
+        skyDivingState = SkyDivingStateENUM.freeFalling;
+        //
+    }
+
+    private void FreeFalling()
+    {
+        // read inputs
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+
+
+    }
+
+    void Update()
+    {
+        switch (skyDivingState)
+        {
+            case SkyDivingStateENUM.startFreeFalling:
+                StartFreeFalling();
+                break;
+
+            case SkyDivingStateENUM.freeFalling:
+                ;
+                break;
+
+            default:
+                break;
+        }
+    }//end Update()
+        //anim.SetBool("SkyDive", false);
 
         /*
 		if (Input.GetAxisRaw ("Mouse Y") > 0)
@@ -74,26 +137,26 @@ public class SkyDiveTesting : MonoBehaviour
         
     */
 
-        //Skydiving
-        //Key left, key right = rotate on the z axis
-        if (Input.GetAxisRaw("Vertical") > 0)
-        {
-            //Dive
-            var angle = transform.rotation.eulerAngles.x;
-            if ((angle < MaxDownAngle) || (angle > 180.0f))
-                transform.Rotate(Vector3.right * RotationSpeed * Time.deltaTime);
-        }
-        else if (Input.GetAxisRaw("Vertical") < 0)
-        {
-            //Pull Back
-            var angle = transform.rotation.eulerAngles.x;
-            if ((angle < MaxUpAngle) && (angle < 180.0f))
-                transform.Rotate(Vector3.right * -(RotationSpeed * Time.deltaTime));
-               // transform.Rotate(Vector3.right * -(RotationSpeed * Time.deltaTime));
-            //transform.Rotate((Vector3.right.x * -(RotationSpeed * Time.deltaTime)), transform.rotation.y, transform.rotation.z);
-        }
+        ////Skydiving
+        ////Key left, key right = rotate on the z axis
+        //if (Input.GetAxisRaw("Vertical") > 0)
+        //{
+        //    //Dive
+        //    var angle = transform.rotation.eulerAngles.x;
+        //    if ((angle < MaxDownAngle) || (angle > 180.0f))
+        //        transform.Rotate(Vector3.right * RotationSpeed * Time.deltaTime);
+        //}
+        //else if (Input.GetAxisRaw("Vertical") < 0)
+        //{
+        //    //Pull Back
+        //    var angle = transform.rotation.eulerAngles.x;
+        //    if ((angle < MaxUpAngle) && (angle < 180.0f))
+        //        transform.Rotate(Vector3.right * -(RotationSpeed * Time.deltaTime));
+        //       // transform.Rotate(Vector3.right * -(RotationSpeed * Time.deltaTime));
+        //    //transform.Rotate((Vector3.right.x * -(RotationSpeed * Time.deltaTime)), transform.rotation.y, transform.rotation.z);
+        //}
 
-        transform.Rotate(Vector3.up * (Input.GetAxisRaw("Vertical") * 10.0f) * Time.deltaTime);
+        //transform.Rotate(Vector3.up * (Input.GetAxisRaw("Vertical") * 10.0f) * Time.deltaTime);
 
         /*
 
@@ -120,14 +183,14 @@ public class SkyDiveTesting : MonoBehaviour
         */
 
 
-    }
+    //}
 
 	private float GetDistanceToTerrain()
 	{
         float distanceToLanding = 999999.0f;
 		RaycastHit hit;
 
-		if (Physics.Linecast(transform.position, Vector3.down, out hit)) {
+		if (Physics.Linecast(cameraPivotTransform.position, Vector3.down, out hit)) {
 			distanceToLanding = hit.distance;
 		}
 
@@ -138,27 +201,43 @@ public class SkyDiveTesting : MonoBehaviour
 	{
         float Cosmic;
         Cosmic = 0.025f;
-		return (Mathf.Abs(90f - transform.rotation.eulerAngles.x) * Cosmic) * FallingDragTuning;
+		return (Mathf.Abs(90f - cameraPivotTransform.rotation.eulerAngles.x) * Cosmic) * FallingDragTuning;
 	}
 
-  void FixedUpdate()
+    private void HandleCameraMovement()
+    {
+        if (Input.GetAxis("Mouse Y") != 0)
+        {
+            _LocalRotation.y -= Input.GetAxis("Mouse Y") * MouseSensitivity;
+            _LocalRotation.y = Mathf.Clamp(_LocalRotation.y, -90f, 90f);
+            Quaternion QT = Quaternion.Euler(_LocalRotation.y, 0, 0);
+            cameraPivotTransform.rotation = Quaternion.Lerp(cameraPivotTransform.rotation, QT, Time.deltaTime * OrbitDampening);
+        }
+        if(Input.GetAxis("Mouse X") != 0)
+        {
+            _LocalRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
+            Quaternion QT = Quaternion.Euler(0, _LocalRotation.x, 0);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, QT, Time.deltaTime * OrbitDampening);
+        }
+    }
+
+    void FixedUpdate()
 	{
         distanceToTerrain = GetDistanceToTerrain();
-
-		if (charFalling)
+        switch (skyDivingState)
         {
-		  if (distanceToTerrain <= ChuteHeight)
-            {
-                OpenChute();
-			  
-		  }
-          else
-          {
-            
-		    rb.drag = CalculateDragIncrement();
-		  }
-		}
-	}
+            case SkyDivingStateENUM.startFreeFalling:
+                break;
+
+            case SkyDivingStateENUM.freeFalling:
+                HandleCameraMovement();
+                FreeFalling();
+                break;
+
+            default:
+                break;
+        }
+    }
 
     //if PLAYERSTATEENUM == freefalling
     //freefall()
@@ -172,7 +251,7 @@ public class SkyDiveTesting : MonoBehaviour
     private void OpenChute()
     {
         rb.drag = ChuteDrag;
-        charFalling = false;
+        //charFalling = false;
         //TODO 
         //animateCute
         //change controls
