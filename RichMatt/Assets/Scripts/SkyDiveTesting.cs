20adb02e281a5abdfb6fadb73206d0b4ec4aa2b6
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-11-30 23:40:37 -0800

    Yaw, Pitch, and Roll all integrated!!!
    three axes all independant of each other!
    roll unwind is broken
    
    deprecated (angle / 360) values and replaced with raw input that matches what you see in Inspector accurately
    
    added comments
    deleted Start()
    deleted swoop input clamping
    deleted roll input clamping
    replaced with Quaternion clamping
    
    new Quanternion clamping function
    functions now require min and max inputs to be reusable
    
    renamed Skeleton variables to Swoop convention
    renamed Pitch variables to Swoop convention
    
    renamed prefab hierarchy and added swoop axis

--- RichMatt/Assets/Scripts/SkyDiveTesting.cs
+++ RichMatt/Assets/Scripts/SkyDiveTesting.cs
@@ -14,23 +14,26 @@ public class SkyDiveTesting : MonoBehaviour
     public float SwoopDrag = 0.01f;
     public float ChuteDrag = 0.5f;
     public float ChuteHeight = 100f;
-    public float PitchChangeSpeed = 145f;
+    public float SwoopChangeSpeed = 145f;
     public float rollChangeSpeed = 5f;
     public float ForwardSpeed = 5f;//player moves forward while falling
     private float dragSmooth = 10f;
 
+    private readonly float minSwoopAngle = -15f;
+    private readonly float maxSwoopAngle =  30f;
+    //private readonly float minSwoopAngle = (-15f / 360f);//deprecated
+    //private readonly float maxSwoopAngle = (30f / 360f);//deprecated
 
-    private readonly float minSwoopAngle = (-15f / 360f);
-    private readonly float maxSwoopAngle = (30f / 360f);
-    
-    private readonly float minRollRotation = (-45f / 360f);
-    private readonly float maxRollRotation = (45f / 360f);
+    private readonly float minRollRotation = -45f;
+    private readonly float maxRollRotation =  45f;
+    //private readonly float minRollRotation = (-45f / 360f);
+    //private readonly float maxRollRotation = (45f / 360f);
 
     //private readonly float _CameraDistance = 10f;
-    public Transform cameraPivotTransform;
-    private Transform characterTransform;
-    public Transform characterSkeletonTransform;
-    public float tempCharacterRotationY;
+    public Transform cameraPivotTransform; //camera look
+    private Transform characterTransform;//this object
+    public Transform characterSwoopTransform; //used for pitch
+    public Transform characterRollAxis;//used for rolling
 
 
     [Header("Camera Controls")]
@@ -43,7 +46,7 @@ public class SkyDiveTesting : MonoBehaviour
 
     //Camera settings
     [SerializeField] private bool smoothCamera = true;
-    private bool clampVerticalRotation = true;
+    private readonly bool clampVerticalRotation = true;
     private readonly float LookMinimumX = -90;
     private readonly float LookMaximumX = 90;
 
@@ -58,9 +61,8 @@ public class SkyDiveTesting : MonoBehaviour
 
     private Quaternion m_CharacterTargetRot;
     private Quaternion m_CameraTargetRot;
-    private Quaternion m_CharacterSkeletonTargetRot;
-
-    private Vector3 m_CharacterSkeletonLocalRot;
+    private Quaternion m_CharacterSwoopTargetRot;
+    private Quaternion m_CharacterRollTargetRot;
 
     #endregion
 
@@ -70,8 +72,8 @@ public class SkyDiveTesting : MonoBehaviour
         characterTransform = this.transform;
         m_CharacterTargetRot = characterTransform.transform.localRotation;
         m_CameraTargetRot = cameraPivotTransform.localRotation;
-        m_CharacterSkeletonTargetRot = characterSkeletonTransform.localRotation;
-        m_CharacterSkeletonLocalRot = Vector3.zero;
+        m_CharacterSwoopTargetRot = characterSwoopTransform.localRotation;
+        m_CharacterRollTargetRot = characterRollAxis.localRotation;
     }
 
     private void Awake()
@@ -87,16 +89,6 @@ public class SkyDiveTesting : MonoBehaviour
 
     void Start()
     {
-
-
-        //rb.AddForce(new Vector3(0, 0, 1), ForceMode.Impulse);
-        //rb.velocity = (transform.forward * 1.5f);
-
-        ////normalize these variables - this permits the level Designer to be carefree about setting these
-        //if (MaxDownAngle > 89.5f)
-        //    MaxDownAngle = (90 - MaxDownAngle) + 1;
-        //MaxUpAngle = Mathf.Abs(MaxUpAngle);
-        //MaxDownAngle = Mathf.Abs(MaxDownAngle);
     }
 
     public void BeginSkyDive()
@@ -156,66 +148,66 @@ public class SkyDiveTesting : MonoBehaviour
     private void RotateView()
     {
         float cameraRotationX = Input.GetAxis("Mouse Y") * MouseYSensitivity;
-        float characterRotationX = Input.GetAxis("Vertical") * PitchChangeSpeed;
+        float characterRotationX = Input.GetAxis("Vertical") * SwoopChangeSpeed;
         float characterRotationY = Input.GetAxis("Mouse X") * MouseXSensitivity;
-        //float characterRotationZ = Input.GetAxis("Horizontal") * rollChangeSpeed;
+        float characterRotationZ = Input.GetAxis("Horizontal") * rollChangeSpeed;
 
-        #region Swoop Clamp
-        if (System.Math.Abs(characterRotationX) < Mathf.Epsilon)
+        float charRoll = characterRollAxis.localRotation.z;
+        float charSwoop = characterSwoopTransform.localRotation.x;
+
+        //unwind swoop amount
+        //if input in deadzone and swoop axis not at identity
+        if (System.Math.Abs(characterRotationX) < Mathf.Epsilon && System.Math.Abs(charSwoop) > .01f)
         {
-            characterRotationX = characterSkeletonTransform.localRotation.x > 0 ? -1 : 1;
+            characterRotationX = charSwoop > 0 ? -1 : 1;
         }
-        else if (characterSkeletonTransform.localRotation.x > maxSwoopAngle)
-        {
-            if (characterRotationX > 0)
-                characterRotationX = 0;
-        }
-        else if (characterSkeletonTransform.localRotation.x < minSwoopAngle)
-        {
-            if (characterRotationX < 0)
-                characterRotationX = 0;
-        }
-        #endregion
 
         #region Clamp Roll
-        //if (System.Math.Abs(characterRotationZ) < Mathf.Epsilon)
+        //unwind
+        //if input in deadzone and roll axis not at identity
+        if (System.Math.Abs(characterRotationZ) < Mathf.Epsilon && System.Math.Abs(charRoll) > .01f)
+        {
+            characterRotationZ = charRoll > 0 ? -1 : 1;
+            
+        }
+        //else if (charRoll > maxRollRotation)
         //{
-        //    if(characterSkeletonTransform.localRotation.z > 0)
-        //    {
-        //        characterRotationZ = 1;
-        //    }
-        //    else if(characterSkeletonTransform.localRotation.z < 0)
-        //    {
-        //        characterRotationZ = -1;
-        //    }
-        //}
-        //else if (characterSkeletonTransform.localRotation.z > maxRollRotation)
-        //{
-        //    if(characterRotationZ < 0)
-        //        characterRotationZ = 0;
-        //}
-        //else if (characterSkeletonTransform.localRotation.z < minRollRotation)
-        //{
+        //    Debug.Log(charRoll + " / " + maxRollRotation);
         //    if (characterRotationZ > 0)
         //        characterRotationZ = 0;
         //}
+        //else if (charRoll < minRollRotation)
+        //{
+        //    if (characterRotationZ < 0)
+        //        characterRotationZ = 0;
+        //}
         #endregion
 
-        m_CharacterTargetRot *= Quaternion.Euler(0f, characterRotationY, 0f);
-        m_CameraTargetRot *= Quaternion.Euler(-cameraRotationX, 0f, 0f);
-        m_CharacterSkeletonTargetRot *= Quaternion.Euler(characterRotationX, 0f, 0f);
-        //m_CharacterSkeletonTargetRot *= Quaternion.Euler(characterRotationX, tempCharacterRotationY, -characterRotationZ);
+        //set target rotations for axes
+        m_CharacterSwoopTargetRot *= Quaternion.Euler(characterRotationX, 0f, 0f);//pitch
+        m_CharacterTargetRot *= Quaternion.Euler(0f, characterRotationY, 0f);//yaw
+        m_CharacterRollTargetRot *= Quaternion.Euler(0f, 0f, -characterRotationZ);//roll
 
+        m_CameraTargetRot *= Quaternion.Euler(-cameraRotationX, 0f, 0f);//camera pitch
+
+        //CLAMP 'EM ALL!
         if (clampVerticalRotation)
         {
-            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot);
+            m_CameraTargetRot = ClampRotationAroundXAxis(m_CameraTargetRot, LookMinimumX, LookMaximumX);
         }
+        m_CharacterSwoopTargetRot = ClampRotationAroundXAxis(m_CharacterSwoopTargetRot, minSwoopAngle, maxSwoopAngle);
+        m_CharacterRollTargetRot = ClampRotationAroundZAxis(m_CharacterRollTargetRot, minRollRotation, maxRollRotation);
     }
     private void HandlePlayerMovement()
     {
-        //move character skeleton
-        characterSkeletonTransform.localRotation = Quaternion.Slerp(characterSkeletonTransform.localRotation,
-            m_CharacterSkeletonTargetRot,
+        //move character pitch
+        characterSwoopTransform.localRotation = Quaternion.Slerp(characterSwoopTransform.localRotation,
+            m_CharacterSwoopTargetRot,
+            smoothTime * Time.deltaTime);
+
+        //move character roll
+        characterRollAxis.localRotation = Quaternion.Slerp(characterRollAxis.localRotation,
+            m_CharacterRollTargetRot,
             smoothTime * Time.deltaTime);
     }
 
@@ -223,23 +215,23 @@ public class SkyDiveTesting : MonoBehaviour
     {
         if (smoothCamera)
         {
-            //move whole character (inluding cam)
+            //move whole character (inluding cam) yaw
             characterTransform.localRotation = Quaternion.Slerp(characterTransform.localRotation, m_CharacterTargetRot,
                 smoothTime * Time.deltaTime);
-            //move camera
+            //move camera pitch
             cameraPivotTransform.localRotation = Quaternion.Slerp(cameraPivotTransform.localRotation, m_CameraTargetRot,
                 smoothTime * Time.deltaTime);
         }
         else
         {
-            characterTransform.localRotation = m_CharacterTargetRot;
-            cameraPivotTransform.localRotation = m_CameraTargetRot;
+            characterTransform.localRotation = m_CharacterTargetRot;//yaw
+            cameraPivotTransform.localRotation = m_CameraTargetRot;//camera pitch
         }
     }
 
     private void HandleDrag()
     {
-        float myRot = characterSkeletonTransform.localRotation.x;
+        float myRot = characterSwoopTransform.localRotation.x;
         float GoalDrag;
 
         //Debug.Log(myRot + " : " + minSwoopAngle + " / " + maxSwoopAngle);
@@ -371,7 +363,7 @@ public class SkyDiveTesting : MonoBehaviour
         //
     }
 
-    private Quaternion ClampRotationAroundXAxis(Quaternion q)
+    private Quaternion ClampRotationAroundXAxis(Quaternion q, float min, float max)
     {
         q.x /= q.w;
         q.y /= q.w;
@@ -379,11 +371,23 @@ public class SkyDiveTesting : MonoBehaviour
         q.w = 1.0f;
 
         float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
-
-        angleX = Mathf.Clamp(angleX, LookMinimumX, LookMaximumX);
-
+        angleX = Mathf.Clamp(angleX, min, max);
         q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
 
         return q;
     }
+    private Quaternion ClampRotationAroundZAxis(Quaternion q, float min, float max)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleZ = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.z);
+        angleZ = Mathf.Clamp(angleZ, min, max);
+        q.z = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleZ);
+
+        return q;
+    }
+
 }
