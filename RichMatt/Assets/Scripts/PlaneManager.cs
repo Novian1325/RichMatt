6b10fb5a85fd589f74be3c88412dbc052b7f3d56
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-12-04 15:59:58 -0800

    Integrated jumping from plane
    serialized private fields for protection and perfomance
    
    changed ways in which controller checked for players and cargo
    
    changed cargo objects -- split between players and supplies
    
    added ConfigurePlayersOnPlane() func
    
    removed deprecated code
    
    new functionality and funcs to support jumping from plane and moving/chaning camera controls
    
    forces player out

--- RichMatt/Assets/Scripts/PlaneManager.cs
+++ RichMatt/Assets/Scripts/PlaneManager.cs
@@ -2,20 +2,22 @@
 
 public class PlaneManager : MonoBehaviour
 {
-	public int airspeed = 100;
-    public GameObject[] cargo;
-    public bool hasDroppedCargo = false;
-    public GameObject planeCameraHolder;
+    [SerializeField] private int airspeed = 100;
+    [SerializeField] private GameObject[] cargo_Supplies;
+    [SerializeField] private PlayerInPlaneController[] cargo_Players;
+    [SerializeField] private bool hasDroppedCargo = false;
+    [SerializeField] private GameObject planeCameraPivot;//tells player camera which object to pivot/orbit around
+    [SerializeField] private Transform dropSpot;//where does the player appear when they jump out of the plane?
 
     //for debugging statements
-    public bool DEBUG = false;
+    [SerializeField] private bool DEBUG = false;
     //to which drop zone is the plane headed?
     private GameObject targetDropZone;
 
+    private DropTypeENUM dropType;
+
     private static int planeCounter = 0;
-
-    private bool greenLight = false;
-
+    
 
     //public UnityStandardAssets.Characters.FirstPerson.FirstPersonController fpsController;
 
@@ -23,38 +25,68 @@ public class PlaneManager : MonoBehaviour
     {
         //returns true if there is at least one player in cargo
         bool playerIsOnBoard = false;
-        foreach (GameObject burden in cargo)
+        if(cargo_Players != null)//if the array has been initialized
         {
-            if (burden.gameObject.CompareTag("Player"))
+            foreach(PlayerInPlaneController player in cargo_Players)//look through array and see
             {
-                //Debug.Log("Player On Board");
-                playerIsOnBoard = true;
+                if(player != null)//is any slot still active?
+                {
+                    playerIsOnBoard = true;//one case makes it true
+                }
             }
         }
         return playerIsOnBoard;
     }
 
+    public Vector3 GetDropSpot()
+    {
+        return dropSpot.position;
+    }
+
     //this is basically the constructor class
-    public void InitPlane(GameObject incomingTargetDropZone, GameObject[] incomingCargo, int incomingAirSpeed = 100)
+    public void InitPlane(DropTypeENUM incomingDropType, GameObject incomingTargetDropZone, GameObject[] incomingPlayers = null, GameObject[] incomingSupplies = null, int incomingAirSpeed = 100)
     {
         if (DEBUG) Debug.Log("This plane heading towards DZ: " + incomingTargetDropZone);
-        this.cargo = incomingCargo;
+        //initialize member variables
+        this.dropType = incomingDropType;
         this.targetDropZone = incomingTargetDropZone;
+        this.cargo_Supplies = incomingSupplies;
         this.airspeed = incomingAirSpeed;
-        planeCameraHolder.SetActive(CheckIfPlayerOnBoard());
         //gives this plane a name for tracking purposes
-        this.gameObject.name = "Plane Number: " + ++planeCounter;
+        if (DEBUG) this.gameObject.name = "Plane Number: " + ++planeCounter;
 
+        //tell player to reaact to being placed on plane
+        ConfigurePlayersOnPlane(incomingPlayers);
+
+    }
+
+    private void ConfigurePlayersOnPlane(GameObject[] incomingPlayers)
+    {
+        cargo_Players = new PlayerInPlaneController[incomingPlayers.Length];
+        if (incomingPlayers != null)//if the array has been initialized
+        {
+            for(int i = 0; i < incomingPlayers.Length; ++i)//look through array and see
+            {
+                if (incomingPlayers[i] != null)//is any slot still active?
+                {
+                    //add a component to the gameobject and keep a reference to it;
+                    cargo_Players[i] = incomingPlayers[i].AddComponent<PlayerInPlaneController>() as PlayerInPlaneController;
+                    cargo_Players[i].OnEnterPlane(this);//message player that he is now on an airplane, and the player will handle everything else from there
+                    //the playerhandler will need a reference to this plane manager to know when it has entered the drop zone
+                }
+            }
+        }
+
+    }
+
+    public Transform GetCameraPivot()
+    {
+        return this.planeCameraPivot.transform;
     }
     // Use this for initialization
     void Start ()
 	{
-        //play sound
-        //randomly select plane model
-        if(this.planeCameraHolder == null)
-        {
-            Debug.LogError("ERROR! planeCam not set!");
-        }
+
 		
 	}
 	
@@ -63,28 +95,43 @@ public class PlaneManager : MonoBehaviour
 	{
         //move the plane forward using translation
 		transform.position += transform.forward * Time.deltaTime * airspeed;
+        
 	}
 
-    //private void LoadPlayersIntoPlane()//deprecated ; player controls themselves
-    //{
-    //    for (int i = 0; i < cargo.Length; ++i)
-    //    {
-    //        if (cargo[i].CompareTag("Player"))
-    //        {
-    //            GameObject player = cargo[i];
-    //            player.transform.SetParent(this.transform);
-    //            player.transform.position = Vector3.zero;//reset 
-    //            player.GetComponent<MeshRenderer>().enabled = false;
-    //        }
-    //    }
-    //}
+    public void OnPlayerJump(PlayerInPlaneController player)
+    {
+        //remove playerController from list
+        for(int i = 0; i < cargo_Players.Length; ++i)
+        {
+            if(cargo_Players[i] == player)
+            {
+                cargo_Players[i] = null;
+            }
+        }
+    }
+
+    public void ForceOutPlayers()
+    {
+        for (int i = 0; i < cargo_Players.Length; ++i)
+        {
+            if (cargo_Players[i] != null)
+            {
+                Debug.Log("GET OUT OF MY PLANE, " + cargo_Players[i].gameObject.name);
+                cargo_Players[i].ForceJump();
+            }
+        }
+
+    }
 
     private void OnTriggerEnter(Collider otherCollider)
     {
         if(otherCollider.gameObject == targetDropZone)
         {
             if (DEBUG) Debug.Log("Now Entering Target Drop Zone: " + otherCollider.name);
-            greenLight = true;
+            foreach(PlayerInPlaneController player in cargo_Players)
+            {
+                player.OnDropZoneEnter();
+            }
             //drop supplies
             //signify to Players they can drop
         }
@@ -101,10 +148,7 @@ public class PlaneManager : MonoBehaviour
         {
             if (DEBUG) Debug.Log("Now Leaving Target Drop Zone: " + otherCollider.name);
             //Force All Players out (if there are any)
-            if (CheckIfPlayerOnBoard())
-            {
-                //GET OUT OF HERE, Players!
-            }
+            ForceOutPlayers();
         }
 
         //Destroy when leaving boundary
