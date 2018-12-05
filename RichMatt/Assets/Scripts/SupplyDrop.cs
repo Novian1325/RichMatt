4b973a033b2c157dadd129fd4505569b4265cdc4
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-12-04 21:26:26 -0800

    Integrated SupplyDrop
    added new variables and references
    
    new shelled and completed funcs
    
    integrated terminal velocity capping and enforcing
    
    integrated skydiving state machine
    deprecated GetDistanceToGround and used static in-hosue library
    
    integrated forward momentum

--- RichMatt/Assets/Scripts/SupplyDrop.cs
+++ RichMatt/Assets/Scripts/SupplyDrop.cs
@@ -2,43 +2,134 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+ [RequireComponent(typeof(Rigidbody))]
 public class SupplyDrop : MonoBehaviour {
 
     //[SerializeField] private float percent
     [SerializeField] private int terminalVelocity = -18;//should be negative, but will be remedied
+    [SerializeField] private int parachuteTerminalVelocity = -9;
+    [SerializeField] private float forwardMomentum = .03f;
+    [Range(0, 1)]//after the object has fallen this percentage of the distance to the ground, pull the chute
+    [SerializeField] private float deployParachuteDistancePercent = .9f;
+    [SerializeField] private SkyDivingStateENUM freefallingState = SkyDivingStateENUM.freeFalling;
+
     private Vector3 terminalVelocityVector;
 
     private int initialDistanceToGround = 0;//distance from instantiated point to ground
     private Animator anim;
-    private Rigidbody rigidbody;
+    private Rigidbody rb;
+    [SerializeField] private bool parachuteDeployed = false;
+
+    public GameObject[] supplies;//holder variable for supplies. probably scriptable objects or itemManagers
     
+    private void AddIconToMiniMap()
+    {
+        //TODO
+    }
+
+    private void FreeFalling()
+    {
+        //check distance to ground
+        if (PPBRS_Utility.GetDistanceToTerrain(this.transform.position) < deployParachuteDistancePercent * initialDistanceToGround)
+        {
+            DeployParachute();
+
+        }
+
+    }
+
+
+    private void DeployParachute()
+    {
+        //do it!
+        terminalVelocity = parachuteTerminalVelocity;
+        terminalVelocityVector = new Vector3(0, terminalVelocity, 0);//convert to vector3
+        parachuteDeployed = true;
+        freefallingState = SkyDivingStateENUM.parachuting;
+    }
+
+    private static int supplyDropCount = 0;
 
 	// Use this for initialization
 	void Start () {
-        terminalVelocity = -Mathf.Abs(terminalVelocity); //get the absolutely negative value
+        
+        //init variables
+        terminalVelocity = -Mathf.Abs(terminalVelocity); //get the absolutely negative value for downwards velocity
         terminalVelocityVector = new Vector3(0, terminalVelocity, 0);//convert to vector3
+        parachuteTerminalVelocity = -Mathf.Abs(parachuteTerminalVelocity); //get the absolutely negative value for downwards velocity
+        //enforce parachuteTerminalVelocity being lower
+        parachuteTerminalVelocity = parachuteTerminalVelocity > terminalVelocity ? terminalVelocity - 1 : parachuteTerminalVelocity;
+        initialDistanceToGround = PPBRS_Utility.GetDistanceToTerrain(this.transform.position);
+
 
         //snag references
-        this.rigidbody = this.GetComponent<Rigidbody>() as Rigidbody;
+        this.rb = this.GetComponent<Rigidbody>() as Rigidbody;
         this.anim = this.GetComponent<Animator>() as Animator;
+
+        //set name
+        this.gameObject.name = "Supply Drop # " + ++supplyDropCount;//track name
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        switch (freefallingState)
+        {
+            case SkyDivingStateENUM.freeFalling:
+                FreeFalling();
+                break;
+
+            case SkyDivingStateENUM.startparachute:
+                DeployParachute();
+                break;
+
+            case SkyDivingStateENUM.parachuting:
+                break;
+
+            case SkyDivingStateENUM.startLanded:
+                //Destroy parachute
+                break;
+
+            case SkyDivingStateENUM.landed:
+                //do stuff. like wait to be opened
+                break;
+
+            default:
+                break;
+        }
 		
 	}
 
-    private int GetDistanceToGround()
-    {
-        int distanceToGround = 0;
-        //TODO
-        return distanceToGround;
-    }
-
     private void FixedUpdate()
     {
+        //apply physics
+        switch (freefallingState)
+        {
+            case SkyDivingStateENUM.freeFalling:
+                //fall
+            case SkyDivingStateENUM.startparachute:
+                //fall
+            case SkyDivingStateENUM.parachuting:
+                //force forward
+                this.rb.AddForce(Vector3.forward * forwardMomentum, ForceMode.Impulse);
+                break;
+
+            case SkyDivingStateENUM.startLanded:
+                //Destroy parachute
+                break;
+
+            case SkyDivingStateENUM.landed:
+                //do stuff. like wait to be opened
+                break;
+
+            default:
+                break;
+        }
+
         //cap downward velocity
-        this.rigidbody.velocity = this.rigidbody.velocity.y > terminalVelocity ? terminalVelocityVector : this.rigidbody.velocity;
+        this.rb.velocity = this.rb.velocity.y > terminalVelocity ? terminalVelocityVector : this.rb.velocity;
+        
+        Debug.Log("Velocity: " + this.rb.velocity);
     }
 }
