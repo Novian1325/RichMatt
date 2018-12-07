2f6c456a8dab0ed16ba56e3d0689ca80dd6d6562
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-12-06 18:30:20 -0800

    new parachute class

---
+++ RichMatt/Assets/Scripts/Parachute.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parachute : MonoBehaviour {
+
+    private Animator anim;
+
+    public void DeployParachute()
+    {
+        anim.SetTrigger("DeployChute");
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        anim = this.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
