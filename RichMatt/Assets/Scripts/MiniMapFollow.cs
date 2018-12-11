2d84155196b05aef25edf6df2671adeef0518baa
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-12-11 01:30:06 -0800

    Update MiniMapFollow.cs
    removed gubbins; simplified statements; and enforced member hiding with defaults

--- RichMatt/Assets/Scripts/MiniMapFollow.cs
+++ RichMatt/Assets/Scripts/MiniMapFollow.cs
@@ -4,33 +4,31 @@ using UnityEngine;
 
 public class MiniMapFollow : MonoBehaviour
 {
-	private GameObject player;
-    public bool RotateWithPlayer;
-    public float MiniMapHeight;
+	
+    [SerializeField] private bool rotateWithPlayer = true;
+    [SerializeField] private int miniMapHeight = 1000;
     private Transform origParent;
-    private Transform PlayerParent;
+    [SerializeField] private Transform targetTransform;
 
 	// Use this for initialization
 	void Start ()
 	{
-		player = GameObject.FindGameObjectWithTag ("Player");
-        transform.position = new Vector3(player.transform.position.x, MiniMapHeight, player.transform.position.z);
+		targetTransform = targetTransform == null ? GameObject.FindGameObjectWithTag ("Player").transform : targetTransform ;
         origParent = transform.parent;
-        PlayerParent = player.transform;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate ()
 	{
-        if(RotateWithPlayer)
+        if(rotateWithPlayer)
         {
-            transform.SetParent(PlayerParent);
-            transform.localPosition = new Vector3(0, MiniMapHeight, 0);
+            transform.SetParent(targetTransform);
         }
         else
         {
             transform.SetParent(origParent);
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + MiniMapHeight, player.transform.position.z);
         }
-	}
+
+        transform.localPosition = new Vector3(0, miniMapHeight, 0);
+    }
 }
