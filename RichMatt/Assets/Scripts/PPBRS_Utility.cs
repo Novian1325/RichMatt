698ca283b76015c30855932beaee8b87516a449c
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-12-12 02:44:05 -0800

    Update PPBRS_Utility.cs
    added functions

--- RichMatt/Assets/Scripts/PPBRS_Utility.cs
+++ RichMatt/Assets/Scripts/PPBRS_Utility.cs
@@ -1,8 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+public enum Axis
+{
+    X, Y, Z
+}
 
 public static class PPBRS_Utility {
+    
+    public static Quaternion ClampRotationAroundXAxis(Quaternion q, float min, float max)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+        angleX = Mathf.Clamp(angleX, min, max);
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+
+    public static Quaternion ClampRotationAroundZAxis(Quaternion q, float min, float max)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleZ = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.z);
+        angleZ = Mathf.Clamp(angleZ, min, max);
+        q.z = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleZ);
+
+        return q;
+    }
 
     static public int GetDistanceToTerrain(Vector3 flyingBody)
     {
