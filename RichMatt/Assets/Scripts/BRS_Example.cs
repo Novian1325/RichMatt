40c1290c603ac45ca595bf3099d6599a921c1479
Matt <mentallic3d@gmail.com>
2018-12-22 00:11:02 -0500

    Example for custom inspector

---
+++ RichMatt/Assets/Scripts/BRS_Example.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BRS_Example : MonoBehaviour
+{
+    [TextArea]
+    public string sample;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sample = "This is a bunch of text in order to test this cool new feature that will make us lots of money and be really cool.  Hopefully this is enough text so that I can stop typing and we can see it work.  Is it?  Time will tell.";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
