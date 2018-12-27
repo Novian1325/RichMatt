b4313d2cad24ac4076eeb7a202f93c47d351d49d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2018-12-26 18:43:20 -0800

    added tooltips
    also enforced PlaneManager component

--- RichMatt/Assets/Scripts/BRS_PlaneDropManager.cs
+++ RichMatt/Assets/Scripts/BRS_PlaneDropManager.cs
@@ -6,17 +6,25 @@ public class BRS_PlaneDropManager : MonoBehaviour
 {
     //public GameObject EndPointBall;
     [Header("Map Settings")]
-    public Transform planeSpawnBounds;//where can the plane start and stop?
-    public GameObject[] playerDropZones;//how big should the zone be that the plane flies through
-    public GameObject[] supplyDropZones;//list containing all areas the player can drop into
+    [Tooltip("This is the spawn boundary for the airplane. Airplane will spawn at the very edge of this cylinder/sphere")]
+    [SerializeField] private Transform planeSpawnBounds;//where can the plane start and stop?
+    [Tooltip("This is the list of volumes where the players are allowed to jump from the plane.")]
+    [SerializeField] private GameObject[] playerDropZones;//how big should the zone be that the plane flies through
+    [Tooltip("These are the volumes for supply drops. Supplies drop as soon as plane enters this volume.")]
+    [SerializeField] private GameObject[] supplyDropZones;//list containing all areas the player can drop into
 
     [Header("Plane Settings")]
-    public GameObject BRS_PlaneSpawn;//plane object (model) to spawn
+    [Tooltip("This is the plane prefab. Must include a PlaneManager script.")]
+    [SerializeField] private GameObject BRS_PlaneSpawn;//plane object (model) to spawn
 
-    public int planeSpeed_PlayerDrop = 150;
-    public int planeSpeed_SupplyDrop = 300;
+    [Tooltip("The plane's airspeed when carrying Players")]
+    [SerializeField] private int planeSpeed_PlayerDrop = 150;
 
-    public bool DEBUG = true;//if true, prints debug statements
+    [Tooltip("The plane's airspeed when on a supply drop sortie")]
+    [SerializeField] private int planeSpeed_SupplyDrop = 300;
+
+    [Tooltip("Enables Debug.Log statements and persistence of objects for debugging purposes.")]
+    [SerializeField] private bool DEBUG = true;//if true, prints debug statements
     
     private GameObject[] acceptableDropZones;
     private List<GameObject> endpointMarkerList = new List<GameObject>();
@@ -308,6 +316,10 @@ public class BRS_PlaneDropManager : MonoBehaviour
         plane.transform.LookAt(planeEndPoint);//point plane towards endpoint
         //get plane manager
         PlaneManager planeManager = plane.GetComponent<PlaneManager>();
+        if(planeManager == null)
+        {
+            plane.AddComponent<PlaneManager>();//create it if it doesn't exist
+        }
         planeManager.InitPlane(targetDropZone, cargo_Players.ToArray(), cargo_Supplies, planeFlightSpeed);
         cargo_Players.Clear();
         return planeManager;
