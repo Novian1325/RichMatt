f2b2080823bca58059038ea81258b61e4d8244b0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-01-02 00:19:11 -0800

    Update TextureResize.cs
    improved transform

--- RichMatt/Assets/Scripts/TextureResize.cs
+++ RichMatt/Assets/Scripts/TextureResize.cs
@@ -4,16 +4,15 @@ using System.Collections;
 [ExecuteInEditMode]
 public class TextureResize : MonoBehaviour
 {
-    public Transform relativeTransform;
-
-    new private Renderer renderer;
     public float tilingX = 50;
     public float tilingY = 10;
-    Material mat;
+    private Transform relativeTransform;
+    new private Renderer renderer;
     // Use this for initialization
     void Start()
     {
         Debug.Log("Start");
+        relativeTransform = transform;
         renderer = GetComponent<Renderer>();
         UpdateTiling();
     }
@@ -21,14 +20,14 @@ public class TextureResize : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-        
+        //UpdateTiling();//DO NOT LEAVE THIS HERE! ONLY UPDATE TILING WHEN SIZE HAS CHANGED
+
 
     }
 
     private void UpdateTiling()
     {
         Debug.Log("Updating the tiling!");
-        renderer.sharedMaterial.mainTextureScale = new Vector2(tilingX * relativeTransform.localScale.x, tilingY);
+        renderer.sharedMaterial.mainTextureScale = new Vector2(tilingX * relativeTransform.lossyScale.x, tilingY);
     }
 }
