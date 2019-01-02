55569e3347fcf35eba933e975aa69b405daf9e53
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-01-02 00:04:38 -0800

    New TextureResize behavior for zone wall added
    does not look pretty when in use, but works.

---
+++ RichMatt/Assets/Scripts/TextureResize.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class TextureResize : MonoBehaviour
+{
+    public Transform relativeTransform;
+
+    new private Renderer renderer;
+    public float tilingX = 50;
+    public float tilingY = 10;
+    Material mat;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Start");
+        renderer = GetComponent<Renderer>();
+        UpdateTiling();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+
+    }
+
+    private void UpdateTiling()
+    {
+        Debug.Log("Updating the tiling!");
+        renderer.sharedMaterial.mainTextureScale = new Vector2(tilingX * relativeTransform.localScale.x, tilingY);
+    }
+}
