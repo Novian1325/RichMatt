8892d880315e44b198b2f55f694cbb658eb0821e
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-02-17 15:04:54 -0800

    Update BRS_ZoneWallManager.cs
    cut using chaff

--- RichMatt/Assets/Scripts/BRS_ZoneWallManager.cs
+++ RichMatt/Assets/Scripts/BRS_ZoneWallManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(CapsuleCollider))]
 [RequireComponent(typeof(LineRenderer))]
