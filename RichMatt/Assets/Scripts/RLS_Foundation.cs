afe12ff6292de032aeb7cfeabaa90d1cc55bd8b7
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-02-20 14:20:20 -0800

    Update RLS_Foundation.cs
    added tooltips
    added robust debbuging
    
    changed list type to smaller size
    improved readability of variables

--- RichMatt/Assets/Scripts/RLS_Foundation.cs
+++ RichMatt/Assets/Scripts/RLS_Foundation.cs
@@ -3,16 +3,32 @@
 public class RLS_Foundation : MonoBehaviour
 {
     [Header("Spawn Weight (percent)")]
-    [Range(0, 100)]
+
+    [Tooltip("Minimum number of loot spots to spawn. +=0 ")]
     [SerializeField] private int MinSpawnPoints = 1;
-    [Range(1, 100)]
+
+    [Tooltip("Max number of loot spots to spawn. -= total number of spots.")]
     [SerializeField] private int MaxSpawnPoints = 99;
 
-    [SerializeField] private GameObject[] LootSpots;
+    [Tooltip("List of all LootSpots to be controlled by this Foundation.")]
+    [SerializeField] private RLS_LootSpot[] LootSpots;
+
+    [Tooltip("Set to true to see details in the Console.")]
+    [SerializeField] private bool DEBUG = false;
+
+    //track how many foundations exist
+    private static int foundationCounter = 0;
+    //which foundation # is this one?
+    private int foundationID;
 
     // Use this for initialization
     void Start ()
     {
+        foundationID = foundationCounter++;
+
+        //verify input
+        ClampLimits();
+
         SelectLootSpots();
     }
 
@@ -30,46 +46,67 @@ public class RLS_Foundation : MonoBehaviour
         MaxSpawnPoints = Mathf.Clamp(MaxSpawnPoints, 0, LootSpots.Length);
         //min must be between 0 and max
         MinSpawnPoints = Mathf.Clamp(MinSpawnPoints, 0, MaxSpawnPoints);
+
+        if (DEBUG)
+        {
+            Debug.LogFormat("Foundation #{0}: Min: {1} / Max: {2} Spawns", foundationID, MinSpawnPoints, MaxSpawnPoints);
+        }
     }
 
-    private static void NotifyLootSpots(GameObject[] selectedLootSpots)
+    /// <summary>
+    /// Tell given LootSpots they have been chosen.
+    /// </summary>
+    /// <param name="selectedLootSpots"></param>
+    private static void NotifyLootSpots(RLS_LootSpot[] selectedLootSpots)
     {
-      foreach (var spot in selectedLootSpots)
-      {
-        spot.GetComponent<RLS_LootSpot> ().SelectVariant();
-      }
+          foreach (var spot in selectedLootSpots)
+          {
+                spot.SelectVariant();
+          }
     }
 
     public void SelectLootSpots()
     {
-        //verify input
-        ClampLimits();
-        //select which indices will be used 
-        var SelectedSpots = SelectRandomLoot(Random.Range(MinSpawnPoints, MaxSpawnPoints));  //temporarily the 'weight' is static
+        //select how many loot spots will be spawned
+        var numberOfSpawns = Random.Range(MinSpawnPoints, MaxSpawnPoints);
+
+        if (DEBUG)
+        {
+            Debug.LogFormat("Foundation #{0}: Will spawn {1} Loot", foundationID, numberOfSpawns);
+        }
+
+        //get a collection of those Loot Spots
+        var selectedSpawns = GetListOfLootSpawns(numberOfSpawns);  //temporarily the 'weight' is static
         //sort out which loot spots are hidden, and which get items spawned in them
-        NotifyLootSpots(SelectedSpots);
+        NotifyLootSpots(selectedSpawns);
+
+        //Delete unused spawn points
     }
 
     // later add an optional 'weight'
-    public GameObject[] SelectRandomLoot(int numberOfLootSpawns)
+    public RLS_LootSpot[] GetListOfLootSpawns(int numberOfLootSpawns)
     {
-      var rand = new System.Random();
-      var gos = new GameObject[numberOfLootSpawns];
-      var used = new int[numberOfLootSpawns];
-      var totalSpawns = LootSpots.Length;
+          //create a new random number generator
+          var randomNumberGenerator = new System.Random();
+          //create an empty list to hold the selected loot spots
+          var lootSpotArray = new RLS_LootSpot[numberOfLootSpawns];
+          var usedIndicesArray = new int[numberOfLootSpawns];
 
-      for (var n = 0; n < numberOfLootSpawns; ++n)
-      {
-        var next = rand.Next(totalSpawns);
-        //  make sure we do not duplicate items
-        while (System.Array.Exists(used, element => element.Equals(next))) //element => element.Equals(1)
-        {
-          next = rand.Next(totalSpawns);
-        }
-        used[n] = next;
-        gos[n] = LootSpots[next];
-      }
-      return gos;
+          for (var n = 0; n < numberOfLootSpawns; ++n)
+          {
+                //get a new random number
+                var randomIndex = randomNumberGenerator.Next(numberOfLootSpawns);
+                //  make sure we do not duplicate items
+                while (System.Array.Exists(usedIndicesArray, element => element.Equals(randomIndex))) //if this index is already in the list of used indices
+                {
+                    //get a new random number
+                    randomIndex = randomNumberGenerator.Next(numberOfLootSpawns);
+                }
+                usedIndicesArray[n] = randomIndex;
+                lootSpotArray[n] = LootSpots[randomIndex];
+          }
+          
+          return lootSpotArray;
     }
 
 }
