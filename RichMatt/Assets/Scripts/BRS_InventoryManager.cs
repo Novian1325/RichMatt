3a5968948be8d17c8e15b96536698d2738fd2ed0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-02-20 20:31:05 -0800

    new class Inventory Manager

---
+++ RichMatt/Assets/Scripts/BRS_InventoryManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace PolygonPilgrimage.BattleRoyaleKit
+{
+    public class BRS_InventoryManager : MonoBehaviour
+    {
+        private List<Item> inventoryList = new List<Item>();//or whatever base type of item you use in your project
+                                                            // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
