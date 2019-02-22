19fa237ab6c68c0ada321a3acffcea2fc0e1dd0a
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-02-22 08:30:06 -0800

    new enum
    isn't this redundant? Isn't this just they "type" of the class? grr

---
+++ RichMatt/Assets/Scripts/Enums/ItemTypeENUM.cs
@@ -0,0 +1,11 @@
+﻿namespace PolygonPilgrimage.BattleRoyaleKit
+{
+    public enum ItemTypeENUM
+    {
+        Armor,
+        Currency,
+        Item = 0,//basic Item
+        Resource,
+        Weapon
+    }
+}
