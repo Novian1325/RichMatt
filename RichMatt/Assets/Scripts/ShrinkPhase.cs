0e5f3e23806af2b71feb925d66c2ab0a088974ff
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-02-22 18:04:28 -0800

    new struct for shrinkPhases

---
+++ RichMatt/Assets/Scripts/ShrinkPhase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace PolygonPilgrimage.BattleRoyaleKit
+{
+    [System.Serializable]//makes visible in Inspector!
+    public struct ShrinkPhase 
+    {
+        /// <summary>
+        /// How many seconds will pass until the Zone Wall begins shrinking.
+        /// </summary>
+        [Tooltip("How many seconds will pass until the Zone Wall begins shrinking.")]
+        public int secondsUntilShrinkBegins;
+
+        /// <summary>
+        /// How many seconds it takes for the Zone Wall to completely shrink through this shrink phase.
+        /// </summary>
+        [Tooltip("How many seconds it takes for the Zone Wall to completely shrink through this shrink phase.")]
+        public int secondsToFullyShrink;
+
+        /// <summary>
+        /// What radius should the Zone Wall stop shrinking at this phase?
+        /// </summary>
+        [Tooltip("What radius should the Zone Wall stop shrinking at this phase?")]
+        public int shrinkToRadius;
+    }
+}
