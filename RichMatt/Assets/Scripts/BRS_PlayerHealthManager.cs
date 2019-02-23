b321d2f0aecb522f8eb9383fba6e828de00fd2d1
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-02-22 20:40:40 -0800

    Added new Scripts

---
+++ RichMatt/Assets/Scripts/BRS_PlayerHealthManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//---------------------------------------------------------------------------------------------------
+//This script is provided as a part of The Polygon Pilgrimage
+//Subscribe to https://www.youtube.com/user/mentallic3d for more great tutorials and helpful scripts!
+//---------------------------------------------------------------------------------------------------
+public class BRS_PlayerHealthManager : MonoBehaviour
+{
+	[Header("---Player Health Parameters---")]
+	public int startHealth;
+	public int maxHealth;
+	public int minHealth;
+	public bool isAlive; //TODO: Future episode on Player stats and 3rd Person Animations!!
+
+	[Header("---Health UI Slider---")]
+	public GameObject _healthSlider;
+	private Slider healthSlider;
+
+	[Header("---Tutorial Only---")]
+	public GameObject _HealthAmount;
+	public Text healthText;
+
+	// Use this for initialization
+	void Start ()
+	{
+		healthSlider = _healthSlider.GetComponent<Slider> ();
+		healthSlider.maxValue = maxHealth;
+		healthSlider.minValue = minHealth;
+		healthSlider.value = startHealth;
+
+		//Remove after tutorial
+		healthText = _HealthAmount.GetComponent<Text> ();
+	}
+
+	public void ChangeHealth(int changeAmount)
+	{
+		healthSlider.value += changeAmount;
+
+		//Remove after tutorial
+		healthText.text = healthSlider.value.ToString ();
+	}
+}
---
+++ RichMatt/Assets/Scripts/BRS_ZoneDamage.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+//---------------------------------------------------------------------------------------------------
+//This script is provided as a part of The Polygon Pilgrimage
+//Subscribe to https://www.youtube.com/user/mentallic3d for more great tutorials and helpful scripts!
+//---------------------------------------------------------------------------------------------------
+public class BRS_ZoneDamage : MonoBehaviour
+{
+	[Header("---Player Object---")]
+	public GameObject player;
+	private string playername;
+
+	[Header("---Post Processing Objects---")]
+	public PostProcessingProfile outsideZonePPP;
+	public PostProcessingProfile standardPPP;
+	private PostProcessingBehaviour CamPPB;
+
+	[Header("---Zone Damage Parameters---")]
+	public float TickRate = 3.0f;
+	public int TickDamage = 1;
+
+	//Future values
+	//For each succesive zone change we will damage
+	//the player more for being outside the zone
+	//public int zoneDamageMultiplier;
+
+	[Header("---Player Health Manager Handle---")]
+	public GameObject _BRS_Mechanics;
+	public BRS_PlayerHealthManager _PHM;
+
+	//Are we in the Zone?
+	private bool inZone;
+
+	//For DEMO purposes ONLY.  Reset our Health to full when we
+	//re-enter the Zone!
+	private bool DebugHealth = true;
+
+	void Start ()
+	{
+		//Get a handle to the Player
+		player = GameObject.FindGameObjectWithTag ("Player");
+		CamPPB = GameObject.Find ("FirstPersonCharacter").GetComponent<PostProcessingBehaviour> ();
+		playername = player.transform.name;
+
+		//Get a handle to the Player Health Manager
+		_PHM = _BRS_Mechanics.GetComponent<BRS_PlayerHealthManager> ();
+
+		//Setup the DamagePlayer to run every X seconds
+		InvokeRepeating ("DamagePlayer", 0.0f, TickRate);
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		//If we leave the zone, we will be damaged!
+		if (col.transform.name == playername)
+		{
+			CamPPB.profile = outsideZonePPP;
+			inZone = false;
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		//If we are inside the zone, all is good!
+		if (col.transform.name == playername)
+		{
+			CamPPB.profile = standardPPP;
+			inZone = true;
+		}
+	}
+
+	void DamagePlayer()
+	{
+		if (!inZone)
+		{
+			//Damage the player [TickDamage] amount
+			_PHM.ChangeHealth (-TickDamage);
+		}
+		else if(DebugHealth)
+		{
+			_PHM.ChangeHealth (100);
+		}
+	}
+}
