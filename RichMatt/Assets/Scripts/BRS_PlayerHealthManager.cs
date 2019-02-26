954b15a9d0dead6daaaeca3e1e61e9708d931674
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-02-25 16:06:11 -0800

    Update BRS_PlayerHealthManager.cs
    added to namespace

--- RichMatt/Assets/Scripts/BRS_PlayerHealthManager.cs
+++ RichMatt/Assets/Scripts/BRS_PlayerHealthManager.cs
@@ -1,44 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 //---------------------------------------------------------------------------------------------------
 //This script is provided as a part of The Polygon Pilgrimage
 //Subscribe to https://www.youtube.com/user/mentallic3d for more great tutorials and helpful scripts!
 //---------------------------------------------------------------------------------------------------
-public class BRS_PlayerHealthManager : MonoBehaviour
+
+namespace PolygonPilgrimage.BattleRoyaleKit
 {
-	[Header("---Player Health Parameters---")]
-	public int startHealth;
-	public int maxHealth;
-	public int minHealth;
-	public bool isAlive; //TODO: Future episode on Player stats and 3rd Person Animations!!
+    public class BRS_PlayerHealthManager : MonoBehaviour
+    {
+        [Header("---Player Health Parameters---")]
+        public int startHealth;
+        public int maxHealth;
+        public int minHealth;
+        public bool isAlive; //TODO: Future episode on Player stats and 3rd Person Animations!!
 
-	[Header("---Health UI Slider---")]
-	public GameObject _healthSlider;
-	private Slider healthSlider;
+        [Header("---Health UI Slider---")]
+        public GameObject _healthSlider;
+        private Slider healthSlider;
 
-	[Header("---Tutorial Only---")]
-	public GameObject _HealthAmount;
-	public Text healthText;
+        [Header("---Tutorial Only---")]
+        public GameObject _HealthAmount;
+        public Text healthText;
 
-	// Use this for initialization
-	void Start ()
-	{
-		healthSlider = _healthSlider.GetComponent<Slider> ();
-		healthSlider.maxValue = maxHealth;
-		healthSlider.minValue = minHealth;
-		healthSlider.value = startHealth;
+        // Use this for initialization
+        void Start()
+        {
+            healthSlider = _healthSlider.GetComponent<Slider>();
+            healthSlider.maxValue = maxHealth;
+            healthSlider.minValue = minHealth;
+            healthSlider.value = startHealth;
 
-		//Remove after tutorial
-		healthText = _HealthAmount.GetComponent<Text> ();
-	}
+            //Remove after tutorial
+            healthText = _HealthAmount.GetComponent<Text>();
+        }
 
-	public void ChangeHealth(int changeAmount)
-	{
-		healthSlider.value += changeAmount;
+        public void ChangeHealth(int changeAmount)
+        {
+            healthSlider.value += changeAmount;
 
-		//Remove after tutorial
-		healthText.text = healthSlider.value.ToString ();
-	}
+            //Remove after tutorial
+            healthText.text = healthSlider.value.ToString();
+        }
+    }
 }
