af327eefb91f2db82ff1321b0b68d5dc3c56744b
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-02-26 11:16:48 -0800

    New ScriptableObject
    moved SOs into their own subfolder
    
    New ScriptableObject for ZonePhaseOptions

---
+++ RichMatt/Assets/Scripts/Scriptable Objects/ZoneWall Phase Options/ShrinkPhaseSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace PolygonPilgrimage.BattleRoyaleKit
+{
+    [CreateAssetMenu(fileName = "ShrinkPhaseOption_", menuName = "ScriptableObjects/ShrinkPhaseOption", order = 1)]
+    public class ShrinkPhaseSettings : ScriptableObject
+    {
+        public ShrinkPhase[] shrinkPhases;
+    }
+
+}
