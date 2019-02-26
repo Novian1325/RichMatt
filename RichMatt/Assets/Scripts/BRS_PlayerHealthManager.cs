36c12df150908f36f7d63cfc56bd10553b01b01f
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-02-25 16:21:21 -0800

    Update BRS_PlayerHealthManager.cs
    summaries, comments,
    
    removed tutorial only stuff
    
    added error-checking
    functionalized

--- RichMatt/Assets/Scripts/BRS_PlayerHealthManager.cs
+++ RichMatt/Assets/Scripts/BRS_PlayerHealthManager.cs
@@ -9,38 +9,97 @@ namespace PolygonPilgrimage.BattleRoyaleKit
 {
     public class BRS_PlayerHealthManager : MonoBehaviour
     {
+        /// <summary>
+        /// Amount of health this Manager currently has.
+        /// </summary>
         [Header("---Player Health Parameters---")]
-        public int startHealth;
-        public int maxHealth;
-        public int minHealth;
-        public bool isAlive; //TODO: Future episode on Player stats and 3rd Person Animations!!
+        [SerializeField] private float currentHealth;
 
+        /// <summary>
+        /// Maximum amount of health this Manager is allowed to have at one time.
+        /// </summary>
+        [SerializeField] private int maxHealth;
+
+        /// <summary>
+        /// Is this health manager current health above 0?
+        /// </summary>
+        [SerializeField] private bool isAlive; //TODO: Future episode on Player stats and 3rd Person Animations!!
+
+        /// <summary>
+        /// UI Component to visually show Players their health. Bots do not need one of these.
+        /// </summary>
         [Header("---Health UI Slider---")]
-        public GameObject _healthSlider;
-        private Slider healthSlider;
-
-        [Header("---Tutorial Only---")]
-        public GameObject _HealthAmount;
-        public Text healthText;
-
+        [SerializeField] private Slider healthSlider;
+        
         // Use this for initialization
         void Start()
         {
-            healthSlider = _healthSlider.GetComponent<Slider>();
-            healthSlider.maxValue = maxHealth;
-            healthSlider.minValue = minHealth;
-            healthSlider.value = startHealth;
+            isAlive = true;//start the game alive
 
-            //Remove after tutorial
-            healthText = _HealthAmount.GetComponent<Text>();
+            if (healthSlider)
+            {
+                healthSlider.maxValue = maxHealth;//max
+                healthSlider.minValue = 0;//health cannot go below 0
+                
+                //update UI
+                healthSlider.value = currentHealth;
+            }
         }
 
-        public void ChangeHealth(int changeAmount)
+        /// <summary>
+        /// Change the amount of health the HealthManager has by given amount.
+        /// </summary>
+        /// <param name="changeAmount">Amount of health to add / remove.</param>
+        public void ChangeHealth(float changeAmount)
         {
-            healthSlider.value += changeAmount;
+            currentHealth += changeAmount;
 
-            //Remove after tutorial
-            healthText.text = healthSlider.value.ToString();
+            if (currentHealth <= 0)
+            {
+                isAlive = false;
+                //do death stuff
+            }
+
+            //health cannot be less than 0 or greater than MaxHealth
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+            //update UI
+            if (healthSlider)
+            {
+                healthSlider.value = currentHealth;
+            }
+        }
+
+        /// <summary>
+        /// Sets this health manager to have maximum health. "Full heal".
+        /// </summary>
+        public void SetToMaxHealth()
+        {
+            currentHealth = maxHealth;
+
+            //update UI
+            if (healthSlider)
+            {
+                healthSlider.value = currentHealth;
+            }
+        }
+
+        /// <summary>
+        /// Get the Max Health of this health manager.
+        /// </summary>
+        /// <returns>A copy of the unit's current max health.</returns>
+        public int GetMaxHealth()
+        {
+            return maxHealth;
+        }
+
+        /// <summary>
+        /// Increase or decrease the unit's max health. Level-Up! or new equipment might change this amount.
+        /// </summary>
+        /// <param name="amount"></param>
+        public void ChangeMaxHealth(int amount)
+        {
+            maxHealth += amount;
         }
     }
 }
